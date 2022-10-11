8f2bb026916d932a802c16e1b6fe7f416b0add1c
Alycia Canavan <alycia.canavan@gmail.com>
2022-10-11 13:17:50 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import backgroundVideo from './backgroundVideo.mp4';
+import React, {Component} from 'react';
+import Ball from './Ball';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      responses: [
+        'It is certain.',
+        'It is decidedly so.',
+        'Without a doubt.',
+        'Yes, definitely.',
+        'You may rely on it.',
+        'As I see it, yes.',
+        'Most likely.',
+        'Outlook good.',
+        'Signs point to yes.',
+        'Reply hazy, try again.',
+        'Ask again later.',
+        'Better not tell you now.',
+        'Cannot predict now.',
+        'Concentrate and ask again.',
+        'Don\'t count on it.',
+        'My reply is no.',
+        'My sources say no.',
+        'Outlook not so good.',
+        'Very doubtful.'
+      ]
+    }
+  }
+  render() {
+    return (
+      <main className="App">
+        <video autoPlay muted loop id="myvideo" preload="auto">
+          <source src={backgroundVideo} type="video/mp4" />
+        </video>
+        <h1>Magic 8 Ball</h1>
+        <h2>Type your question below and give it a shake.</h2>
+        <Ball {this.state} />
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/Ball.js
@@ -0,0 +1,12 @@
+import React from "react";
+import magicBall from './magicBall.png';
+import './Ball.css';
+
+function Ball(props) {
+    console.log(props.responses);
+    return (
+        <div className='ballSection'>Hi!</div>
+    )
+}
+
+export default Ball;
